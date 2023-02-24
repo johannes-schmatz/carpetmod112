7e8bf3cc4cf191111fb837a6ce6c613ef37ee6bb
Johannes Schmatz <johannes_schmatz@gmx.de>
2023-02-24 23:14:13 +0100

    Fix sky's mixin using Shift.BY with 2, but still not hitting the correct bytecode

--- src/main/java/carpet/mixin/player/ServerWorldMixin.java
+++ src/main/java/carpet/mixin/player/ServerWorldMixin.java
@@ -7,13 +7,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.UUID;
 
 @Mixin(ServerWorld.class)
 public class ServerWorldMixin {
-    @Inject(
+    /*@Inject(
             method = "method_12781",
             at = @At(
                     value = "INVOKE_ASSIGN",
@@ -26,6 +23,21 @@ public class ServerWorldMixin {
             locals = LocalCapture.CAPTURE_FAILHARD
     )
     private void loginMinecartFix(Entity entityIn, CallbackInfoReturnable<Boolean> cir, UUID uuid, Entity entity) {
+        if (FakeServerPlayerEntity.shouldFixMinecart()) {
+            entity.removed = true;
+            cir.setReturnValue(true);
+        }
+    }*/
+
+    @Inject(
+            method = "method_12781",
+            at = @At(
+                    value = "CONSTANT",
+                    args = "classValue=net/minecraft/entity/player/PlayerEntity"
+            ),
+            cancellable = true
+    )
+    private void onPlayerEntityClass(Entity entity, CallbackInfoReturnable<Boolean> cir) {
         if (FakeServerPlayerEntity.shouldFixMinecart()) {
             entity.removed = true;
             cir.setReturnValue(true);
